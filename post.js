573f67704039be1dd345a6959b4b1aef00f01b7b
Sebastian Triana martinez <sebastian.triana@utp.edu.co>
2026-03-11 13:14:50 -0500

    added media sharing bar

--- post.js
+++ post.js
@@ -71,20 +71,16 @@ function renderShareBar(post) {
             '<span class="post-share-label">Compartir</span>' +
 
             '<a class="post-share-btn" href="https://www.facebook.com/sharer/sharer.php?u=' + url + '" target="_blank">' +
-                facebookIcon() +
+                '<img src="https://cdn.simpleicons.org/facebook" alt="Facebook">' +
             '</a>' +
 
             '<a class="post-share-btn" href="https://twitter.com/intent/tweet?url=' + url + '&text=' + text + '" target="_blank">' +
-                xIcon() +
+                '<img src="https://cdn.simpleicons.org/x" alt="X">' +
             '</a>' +
 
             '<a class="post-share-btn" href="https://wa.me/?text=' + text + '%20' + url + '" target="_blank">' +
-                whatsappIcon() +
+                '<img src="https://cdn.simpleicons.org/whatsapp" alt="WhatsApp">' +
             '</a>' +
-
-            '<button class="post-share-btn" onclick="copyPostLink()">' +
-                linkIcon() +
-            '</button>' +
 
         '</div>'
     );
@@ -137,36 +133,5 @@ function updateMeta(post) {
 
 }
 
-function facebookIcon() {
-return `
-<svg viewBox="0 0 24 24" width="20" height="20">
-<path fill="currentColor" d="M22 12a10 10 0 10-11.5 9.9v-7h-2.9V12h2.9V9.8c0-2.9 1.7-4.5 4.3-4.5 1.2 0 2.5.2 2.5.2v2.7H16c-1.4 0-1.9.9-1.9 1.8V12h3.2l-.5 2.9H14v7A10 10 0 0022 12z"/>
-</svg>
-`;
-}
-
-function xIcon() {
-return `
-<svg viewBox="0 0 24 24" width="20" height="20">
-<path fill="currentColor" d="M18.9 2H22l-7.2 8.2L23 22h-6.4l-5-6.5L5.8 22H2.7l7.7-8.8L1 2h6.6l4.6 6.1L18.9 2z"/>
-</svg>
-`;
-}
-
-function whatsappIcon() {
-return `
-<svg viewBox="0 0 24 24" width="20" height="20">
-<path fill="currentColor" d="M20.5 3.5A11.9 11.9 0 0012 .5C5.7.5.5 5.7.5 12c0 2.1.6 4.1 1.7 5.9L.5 23.5l5.8-1.6A11.8 11.8 0 0012 23.5c6.3 0 11.5-5.2 11.5-11.5 0-3.1-1.2-6-3.5-8.5zM12 21.4c-1.9 0-3.7-.5-5.3-1.5l-.4-.2-3.4.9.9-3.3-.3-.4A9.4 9.4 0 012.6 12c0-5.2 4.2-9.4 9.4-9.4 2.5 0 4.9 1 6.7 2.8a9.3 9.3 0 012.8 6.6c0 5.2-4.3 9.4-9.5 9.4z"/>
-</svg>
-`;
-}
-
-function linkIcon() {
-return `
-<svg viewBox="0 0 24 24" width="20" height="20">
-<path fill="currentColor" d="M3.9 12a5 5 0 015-5h3v2h-3a3 3 0 000 6h3v2h-3a5 5 0 01-5-5zm7-1h2v2h-2v-2zm4.2-4h-3v2h3a3 3 0 010 6h-3v2h3a5 5 0 000-10z"/>
-</svg>
-`;
-}
 
 document.addEventListener('DOMContentLoaded', loadPost);
